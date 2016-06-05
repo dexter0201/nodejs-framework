8933269048b9d6ed625e5a22300526d48b160b07
Lich Nguyen <jjm.nvl@gmail.com>
2016-06-05 14:39:12 +0700

    Init bower + remove hard-code files

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,17 @@
 window.app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
+        .when('/acticles', {
+            templateUrl: 'views/acticles/list.html'
+        })
+        .when('/acticles/create', {
+            templateUrl: '/views/acticles/create.html'
+        })
+        .when('/acticles/:acticleId/edit', {
+            templateUrl: 'views/acticles/edit.html'
+        })
+        .when('/acticles/:acticleId', {
+            templateUrl: 'views/acticles/view.html'
+        })
         .when('/', {
             templateUrl: 'views/index.html'
         })
