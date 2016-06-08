90ebe1d058812cb3c641df6c210beac64dca9765
Lich Nguyen <jjm.nvl@gmail.com>
2016-06-08 23:15:00 +0700

    Add grunt

---
+++ gruntfile.js
@@ -0,0 +1,82 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        watch: {
+            html: {
+                files: ['public/views/**'],
+                options: {
+                    livereload: true
+                }
+            },
+            js: {
+                files: ['public/js/**'],
+                options: {
+                    livereload: true
+                }
+            },
+            css: {
+                files: ['public/scss/**'],
+                tasks: ['compass'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        jshint: {
+            all: ['gruntfile.js']
+        },
+        compass: {
+            dist: {
+                options: {
+                    sassDir: 'public/scss',
+                    cssDir: 'public/css',
+                    environment: 'production'
+                }
+            },
+            dev: {
+                options: {
+                    sassDir: 'public/scss',
+                    cssDir: 'public/css'
+                }
+            }
+        },
+        nodemon: {
+            dev: {
+                options: {
+                    files: 'server.js',
+                    args: [],
+                    ignoredFiles: ['README.md', 'node_modules/**', '.DS_Store'],
+                    watchedExtensions: ['js'],
+                    watchedFolders: ['app', 'config'],
+                    debug: true,
+                    delayTime: 1,
+                    env: {
+                        PORT: 3000
+                    },
+                    cwd: __dirname
+                }
+            },
+            exec: {
+                options: {
+                    exec: 'less'
+                }
+            }
+        },
+        concurrent: {
+            target: {
+                tasks: ['nodemon', 'watch'],
+                options: {
+                    logConcurrentOutput: true
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-concurrent');
+
+    grunt.registerTask('default', ['jshint', 'compass']);
+};
