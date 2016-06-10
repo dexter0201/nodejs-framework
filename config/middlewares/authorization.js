27bd9a49de7905c38c8420efb928c92916e447ff
Lich Nguyen <jjm.nvl@gmail.com>
2016-06-11 00:42:48 +0700

    Middlewares fix

--- config/middlewares/authorization.js
+++ config/middlewares/authorization.js
@@ -2,8 +2,26 @@
 
 exports.requiresLogin = function (req, res, next) {
     if (!req.isAuthenticated()) {
-        res.redirect('/signin');
+        res.send(401, 'User is not authoried');
     }
 
     next();
+};
+
+exports.user = {
+    hasAuthorization: function (req, res, next) {
+        if (req.profile.id != req.user.id) {
+            res.send(401, 'User is not authoried');
+        }
+        next();
+    }
+};
+
+exports.article = {
+    hasAuthorization: function (req, res, next) {
+        if (req.article.user.id != req.user.id) {
+            res.send(401, 'User is not authoried');
+        }
+        next();
+    }
 };
