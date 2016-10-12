d5fbc05a3f5188c03a321ae73972ae7a1383a20d
Dexter Nguyen <lich.nguyen@amblique.com>
2016-10-12 20:01:17 +0700

    Modify references for restrutering

--- public/js/config.js
+++ public/js/config.js
@@ -42,49 +42,49 @@ angular.module('dexter').config(['$stateProvider', '$urlRouterProvider', '$locat
         $stateProvider
             .state('all articles', {
                 url: '/articles',
-                templateUrl: 'views/articles/list.html',
+                templateUrl: 'articles/views/list.html',
                 resolve: {
                     loggedin: checkLoggedIn
                 }
             })
             .state('create article', {
                 url: '/articles/create',
-                templateUrl: '/views/articles/create.html',
+                templateUrl: 'articles/views/create.html',
                 resolve: {
                     loggedin: checkLoggedIn
                 }
             })
             .state('edit article', {
                 url: '/articles/:articleId/edit',
-                templateUrl: 'views/articles/edit.html',
+                templateUrl: 'articles/views/edit.html',
                 resolve: {
                     loggedin: checkLoggedIn
                 }
             })
             .state('view an article', {
                 url: '/articles/:articleId',
-                templateUrl: 'views/articles/view.html',
+                templateUrl: 'articles/views/view.html',
                 resolve: {
                     loggedin: checkLoggedIn
                 }
             })
             .state('home', {
                 url: '/',
-                templateUrl: 'views/index.html'
+                templateUrl: 'system/views/index.html'
             })
             .state('auth', {
-                templateUrl: 'views/auth/index.html'
+                templateUrl: 'auth/views/index.html'
             })
             .state('auth.login', {
                 url: '/login',
-                templateUrl: 'views/auth/login.html',
+                templateUrl: 'auth/views/login.html',
                 resolve: {
                     loggedin: checkLoggedOut
                 }
             })
             .state('auth.register', {
                 url: '/register',
-                templateUrl: '/views/auth/register.html',
+                templateUrl: 'auth/views/register.html',
                 resolve: {
                     loggedin: checkLoggedOut
                 }
