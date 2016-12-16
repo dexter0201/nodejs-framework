db8671fa6a03a117fc092b29b17be97938682399
Dexter Nguyen <Dexter@Dexters-MacBook-Pro.local>
2016-12-16 10:34:56 +0700

    Added ThemeController scope

--- packages/theme/public/controllers/theme.js
+++ packages/theme/public/controllers/theme.js
@@ -1,7 +1,22 @@
 'use strict';
 
-angular.module('dexter.theme').controller('ThemeController', ['$scope',
-    function ($scope) {
+angular.module('dexter.theme').controller('ThemeController', ['$scope', 'Global', '$location', '$rootScope',
+    function ($scope, Global, $location, $rootScope) {
 
+        $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
+            var elements = $location.path().split('/');
+            var lastElement = elements.slice('-1')[0];
+
+            $scope.state = toState;
+
+            if ($scope.state === '') {
+                $scope.state = 'firstPage';
+            }
+        });
+
+        $scope.global = Global;
+        $scope.package = {
+            name: 'theme'
+        };
     }
 ]);
