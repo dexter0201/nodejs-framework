8d05fd739c8ad352e4cba36fb30cd8508bae01fb
Dexter Nguyen <Dexter@Dexters-MacBook-Pro.local>
2016-12-16 16:03:24 +0700

    Increase nodejscore module

--- packages/theme/app.js
+++ packages/theme/app.js
@@ -6,6 +6,7 @@ var Theme = new Module('theme');
 Theme.register(function (app, auth, database, passport) {
     Theme.routes(app, auth, database, passport);
     Theme.aggregateAsset('css', 'loginForms.css');
+    Theme.aggregateAsset('css', 'theme.css');
 
     return Theme;
 });
