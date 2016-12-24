e06e09bbc00bdc65b01288e98fb9976f71b36b92
Dexter Nguyen <lich.nguyen@amblique.com>
2016-12-25 00:27:14 +0700

    Fix rootscope state issue

--- packages/system/public/system.js
+++ packages/system/public/system.js
@@ -1,4 +1,18 @@
 'use strict';
 
 //angular.module('dexter', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.router', 'dexter.system', 'dexter.articles', 'dexter.auth']);
-angular.module('dexter.system', ['ui.router', 'dexter-factory-interceptor']);
+angular.module('dexter.system', ['ui.router', 'dexter-factory-interceptor'])
+    .run(['$rootScope', function ($rootScope) {
+        $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams){
+            var toPath = toState.url;
+
+            toPath = toPath.replace(new RegExp('/', 'g'), '');
+            toPath = toPath.replace(new RegExp(':', 'g'),'-');
+            $rootScope.state = toPath;
+
+            if ($rootScope.state === '' ) {
+                $rootScope.state = 'firstPage';
+            }
+        });
+    }]
+);
--- packages/theme/public/controllers/theme.js
+++ packages/theme/public/controllers/theme.js
@@ -1,19 +1,7 @@
 'use strict';
 
-angular.module('dexter.theme').controller('ThemeController', ['$scope', 'Global', '$location', '$rootScope',
-    function ($scope, Global, $location, $rootScope) {
-
-        $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
-            var elements = $location.path().split('/');
-            var lastElement = elements.slice('-1')[0];
-
-            $scope.state = toState;
-
-            if ($scope.state === '') {
-                $scope.state = 'firstPage';
-            }
-        });
-
+angular.module('dexter.theme').controller('ThemeController', ['$scope', 'Global',
+    function ($scope, Global) {
         $scope.global = Global;
         $scope.package = {
             name: 'theme'
