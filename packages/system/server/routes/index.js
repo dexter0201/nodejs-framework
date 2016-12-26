68086a2677448730971fa6a156a0777dc977d420
Dexter Nguyen <lich.nguyen@amblique.com>
2016-12-26 19:03:32 +0700

    Added worker

--- packages/system/server/routes/index.js
+++ packages/system/server/routes/index.js
@@ -1,8 +1,15 @@
 'use strict';
 
+var nodejscore = require('nodejscore');
+
 module.exports = function (System, app/*, auth, database*/) {
     var index = require('../controllers/index');
 
     app.route('/')
         .get(index.render);
+
+    app.get('/*', function (req, res, next) {
+        res.header('workerId', JSON.stringify(nodejscore.options.workerid));
+        next();
+    });
 };
--- server.js
+++ server.js
@@ -1,11 +1,38 @@
 'use strict';
 
+const cluster = require('cluster');
 var nodejscore = require('nodejscore');
 
-nodejscore.runInstance({}, function (app, config) {
-    console.log('Dexter\'s app started on port ' + config.http.port + ' (' + process.env.NODE_ENV + ')');
+if (cluster.isMaster &&
+    process.execArgv.indexOf('--debug') < 0 &&
+    process.env.NODE_ENV !== 'test' &&
+    process.execArgv.indexOf('--singleProcess') < 0) {
+    var cpuCount = require('os').cpus().length;
 
-    if (config.https && config.https.port) {
-        console.log('Dexter\'s secure app started on port ' + config.https.port + ' (' + process.env.NODE_ENV + ')');
+    for (var i = 0; i < cpuCount; i++) {
+        console.log('forking ', i);
+        cluster.fork();
     }
-});
+
+    cluster.on('exit', function (worker) {
+        console.log('Worker ', worder.id, ' died :(');
+        cluster.fork();
+    });
+} else {
+    var workerid = !cluster.isMaster ? cluster.worker.id : 0;
+
+    nodejscore.runInstance({
+        workerid: workerid
+    }, function (app, config) {
+        var port = config.https && config.https.port ? config.https.port : config.http.port;
+
+        console.log(
+            'Dexter\'s app started on port ',
+            config.https.port,
+            ' (',
+            process.env.NODE_ENV,
+            ') cluster.worker.id ',
+            workerid
+        );
+    });
+}
