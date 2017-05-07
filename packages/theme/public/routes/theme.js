c97c3161f4ac52a3a020bb091432e3a1869353de
Dexter Nguyen <lich.nguyen@amblique.com>
2017-05-07 15:33:25 +0700

    Added weight into package.json

--- packages/theme/public/routes/theme.js
+++ packages/theme/public/routes/theme.js
@@ -1,11 +1,11 @@
 'use strict';
 
-angular.module('nodejscore.theme').config(['$nodeJsCoreStateProvider',
-    function ($nodeJsCoreStateProvider) {
-        $nodeJsCoreStateProvider
-            .state('theme example page', {
-                url: '/theme example page',
-                templateUrl: 'users/views/index.html'
-            });
-    }
-]);
+// angular.module('nodejscore.theme').config(['$nodeJsCoreStateProvider',
+//     function ($nodeJsCoreStateProvider) {
+//         $nodeJsCoreStateProvider
+//             .state('theme example page', {
+//                 url: '/theme example page',
+//                 templateUrl: 'users/views/index.html'
+//             });
+//     }
+// ]);
